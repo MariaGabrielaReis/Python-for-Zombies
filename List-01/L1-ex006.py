6dd7a0bea92332ce53e22098e6b5d56e1a1c568f
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-02-16 10:49:30 -0300

    :pencil2: Fixing exercise six

--- List-01/L1-ex006.py
+++ List-01/L1-ex006.py
@@ -4,11 +4,11 @@ print("Cálculo de tempo de uma viagem")
 
 d = float(input('Distância a ser percorrida (km): '))
 v = int(input('Velocidade média esperada (km/h): '))
-h = int((d/v))
+h = d/v
 
-if ((d/v) == 1):
-    m = 0
-else:
-    m = (d/v)*60
+if h//1 == h:
+	m = 0
+else: 
+	m = ((d/v) - (d//v))*60
 
 print(f'O tempo esperado da viagem será de {h:.0f} hora(s) e {m:.0f} minuto(s)')
