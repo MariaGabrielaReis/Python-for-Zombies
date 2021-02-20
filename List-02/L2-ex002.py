216de14c3014a3665c820e389d7cdaed995ec9e1
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-02-20 10:46:17 -0300

    :pencil2: Adding other ways to solve exercises 1 and 2

--- List-02/L2-ex002.py
+++ List-02/L2-ex002.py
@@ -1,6 +1,16 @@
 #Exercise 02 - List 02
 #Determine se um ano é bissexto.
 print('Descobrindo se o ano é bissexto')
+# usando biblioteca
+import calendar
+ano = int(input('Digite o ano: '))
+
+if calendar.isleap(ano):
+    print('O ano é bissexto!')
+else:
+    print('O ano não é bissexto :(')
+
+# usando cadeia de condições
 ano = int(input('Digite o ano: '))
 	
 if ano % 4 == 0 or ano % 400 == 0:
